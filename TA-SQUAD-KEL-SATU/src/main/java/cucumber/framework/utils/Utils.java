aa62440b509ff4425498658aeda5313a3ad6670f
novribatara <112675184+novriantobatara@users.noreply.github.com>
2022-09-23 12:47:55 +0700

    Commit awal tugas akhir

---
+++ TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/utils/Utils.java
@@ -0,0 +1,51 @@
+package cucumber.framework.utils;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Utils {
+	private Robot robot;
+	public static int testCount = 0;
+	public static int countOutline = 1;
+	
+	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir")+"/FailedTestScreenshot/"
+				+screenshotName+"_"+dateName+".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+	}
+	
+	public void zoomOut(int n) throws AWTException{
+	  	this.robot = new Robot();
+	  	for (int i = 0; i < n; i++) {			
+	  		robot.keyPress(KeyEvent.VK_CONTROL);
+	  		robot.keyPress(KeyEvent.VK_SUBTRACT);
+	  		robot.keyRelease(KeyEvent.VK_CONTROL);
+	  		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		}
+	}
+	
+	public void zoomIn(int n) throws AWTException{
+	  	this.robot = new Robot();
+	  	for (int i = 0; i < n; i++) {			
+	  		robot.keyPress(KeyEvent.VK_CONTROL);
+	  		robot.keyPress(KeyEvent.VK_ADD);
+	  		robot.keyRelease(KeyEvent.VK_CONTROL);
+	  		robot.keyRelease(KeyEvent.VK_ADD);
+		}
+	}
+}
