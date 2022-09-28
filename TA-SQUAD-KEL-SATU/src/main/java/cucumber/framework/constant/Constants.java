b8781737982d295f5959452b266c9fd02ed36cb5
novribatara <112675184+novriantobatara@users.noreply.github.com>
2022-09-28 22:16:24 +0700

    Update constants and utils

--- TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/constant/Constants.java
+++ TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/constant/Constants.java
@@ -9,8 +9,7 @@ public class Constants {
 	public static final String OPERA = "Opera";
 	public static final String IE = "Internet Explorer";
 	public static final String SAFARI = "Safari";
-	public static final String URL = "https://opensource-demo.orangehrmlive.com";
-	public static final String URL_DEMO_QA = "https://demoqa.com/elements";
+	public static final String URL_SILOAM = "https://dev.ptdika.com/siloam/panel/login/";
 	public static final String URL_NOPC = "https://admin-demo.nopcommerce.com/";
 	public static final String GLOB_PARAM_DELAY = "n";
 }
--- TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/utils/Utils.java
+++ TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/utils/Utils.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -14,7 +15,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class Utils {
-	private Robot robot;
 	public static int testCount = 0;
 	public static int countOutline = 1;
 	
@@ -41,8 +41,8 @@ public class Utils {
 		}		
 	}
 	
-	public void zoomOut(int n) throws AWTException{
-	  	this.robot = new Robot();
+	public static void zoomOut(int n) throws AWTException{
+	  	Robot robot = new Robot();
 	  	for (int i = 0; i < n; i++) {			
 	  		robot.keyPress(KeyEvent.VK_CONTROL);
 	  		robot.keyPress(KeyEvent.VK_SUBTRACT);
@@ -51,13 +51,89 @@ public class Utils {
 		}
 	}
 	
-	public void zoomIn(int n) throws AWTException{
-	  	this.robot = new Robot();
+	public static void zoomIn(int n) throws AWTException{
+	  	Robot robot = new Robot();
 	  	for (int i = 0; i < n; i++) {			
 	  		robot.keyPress(KeyEvent.VK_CONTROL);
 	  		robot.keyPress(KeyEvent.VK_ADD);
 	  		robot.keyRelease(KeyEvent.VK_CONTROL);
 	  		robot.keyRelease(KeyEvent.VK_ADD);
 		}
+	}
+	
+	public static int generateInteger(int intFirstIndex, int intLastIndex) {
+		Random rand = new Random();
+		int intTemp = 0;
+		
+		try {
+			intTemp = rand.nextInt(intFirstIndex, intLastIndex+1);			
+		} catch (IllegalArgumentException e) {
+			intTemp = 0;
+		}
+		
+		return intTemp;
+	}
+	
+	public static String generateString(int intStringLength) {
+		int randAlphabeth = 0;
+		final char charBatasBawahAlphabeth = 97;
+		final char charBatasAtasAlphabeth = 123;
+		Random rand = new Random();
+		StringBuffer strRandomString = new StringBuffer();
+		
+		for (int i = 0; i < intStringLength; i++) {
+			randAlphabeth = rand.nextInt(charBatasBawahAlphabeth,charBatasAtasAlphabeth);
+			strRandomString.append((char)randAlphabeth);
+		}
+		
+		return strRandomString.toString();
+	}
+	
+	public static String generateWord(int intWord, int intWordLength) {
+		StringBuffer strRandomString = new StringBuffer();
+		String strTemp = "";
+		
+		for (int i = 0; i < intWord; i++) {
+			strRandomString.append(generateString(intWordLength) + " ");
+		}
+		
+		strTemp = strRandomString.toString().substring(0,strRandomString.length()-1);
+		
+		return strTemp;
+	}
+	
+	public static String generateDate() {
+		Random rand = new Random();
+		String strTemp = "";
+		
+		strTemp = 2022 + "-" + rand.nextInt(1,13) + "-" + rand.nextInt(1,31);
+		
+		return strTemp;
+	}
+	
+	public static String generateDate(int firstIndexYear, int lastIndexYear) {
+		Random rand = new Random();
+		String strTemp = "";
+		
+		try {			
+			strTemp = rand.nextInt(firstIndexYear,lastIndexYear+1) + "-" + rand.nextInt(1,13) + "-" + rand.nextInt(1,31);
+		} catch (Exception e) {
+			strTemp = 2022 + "-" + rand.nextInt(1,13) + "-" + rand.nextInt(1,31);
+		}
+		
+		return strTemp;
+	}
+	
+	public static String generateDate(int firstIndexMonth, int lastIndexMonth, int firstIndexDate, int lastIndexDate) {
+		Random rand = new Random();
+		String strTemp = "";
+		
+		try {			
+			strTemp = 2022 + "-" + rand.nextInt(firstIndexMonth,lastIndexMonth+1) + "-" + rand.nextInt(firstIndexDate,lastIndexDate+1);
+		} catch (Exception e) {
+			strTemp = 2022 + "-" + rand.nextInt(1,13) + "-" + rand.nextInt(1,31);
+		}
+		
+		return strTemp;
 	}
 }
