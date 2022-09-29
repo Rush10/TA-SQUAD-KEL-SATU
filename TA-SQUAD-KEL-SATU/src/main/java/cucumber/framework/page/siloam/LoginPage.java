3261f476f1790ba355a803a43c37de10d18247a1
novribatara <112675184+novriantobatara@users.noreply.github.com>
2022-09-29 10:52:03 +0700

    Forget Commit LoginPage (Corry)

---
+++ TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/page/siloam/LoginPage.java
@@ -0,0 +1,64 @@
+package cucumber.framework.page.siloam;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import cucumber.framework.connection.DriverSingleton;
+import cucumber.framework.utils.Utils;
+import cucumber.framework.constant.Constants;
+
+public class LoginPage {
+	private WebDriver driver;
+	
+	public LoginPage() {
+		this.driver = DriverSingleton.getDriver();
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//input[@name='username']")
+	private WebElement username;
+	
+	@FindBy(xpath = "//input[@name='password']")
+	private WebElement password;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	private WebElement login;
+	
+	@FindBy(xpath = "//div[@class='alert alert-danger alert-dismissable']")
+//	@FindBy(xpath = "//div[@id='page-container']/div/div[2]/div")
+	private WebElement msgError; //pesan error
+	
+	@FindBy(xpath = "//div[@id='sidebar']/div/div/ul/li/a/div[3]")
+	private WebElement lblName; //label name setelah login
+	
+
+	public void login(String user, String pass) {
+		inputUsername(user);
+		Utils.delay(Constants.TIMEOUT_DELAY, Constants.GLOB_PARAM_DELAY);
+		inputPassword(pass);
+		Utils.delay(Constants.TIMEOUT_DELAY, Constants.GLOB_PARAM_DELAY);
+		btnLogin();
+	}
+	
+	public void inputUsername(String user) {
+		this.username.sendKeys(user);
+	}
+	
+	public void inputPassword(String pass) {
+		this.password.sendKeys(pass);
+	}
+	
+	public void btnLogin() {
+		this.login.click();
+	}
+	
+	public String msgInvalid() {
+		return Utils.driverWaitTxt(driver, Constants.TIMEOUT, msgError);
+	}
+	
+	public String getTxtUserInfo() {
+		return Utils.driverWaitTxt(driver, Constants.TIMEOUT, lblName);
+	}
+}
