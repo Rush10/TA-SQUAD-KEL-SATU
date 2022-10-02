fa6ee8284e3d1bfb71898b347908a57ce8cb4f64
Rush10 <novriantobatara@gmail.com>
2022-10-02 13:41:40 +0700

    Update constants

--- TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/constant/Constants.java
+++ TA-SQUAD-KEL-SATU/src/main/java/cucumber/framework/constant/Constants.java
@@ -4,7 +4,7 @@ package cucumber.framework.constant;
 created_by : Novri
 created_date : 21/09/2022
 updated_by : Novri
-updated_date : 29/09/2022
+updated_date : 02/10/2022
 */
 
 public class Constants {
@@ -12,12 +12,18 @@ public class Constants {
 	public static final int TIMEOUT = 10;
 	public static final int TIMEOUT_DELAY = 2;
 	public static final String GLOB_PARAM_DELAY = "y";
+	
 	public static final String CHROME = "Chrome";
 	public static final String FIREFOX = "Firefox";
 	public static final String EDGE = "Edge";
 	public static final String OPERA = "Opera";
 	public static final String IE = "Internet Explorer";
 	public static final String SAFARI = "Safari";
+	
 	public static final String URL_SILOAM = "https://dev.ptdika.com/siloam/panel/login/";
-	public static final String URL_NOPC = "https://admin-demo.nopcommerce.com/";
+	
+	public static final String USERNAME_ADMIN_SILOAM = "admindika";
+	public static final String PASSWORD_ADMIN_SILOAM = "d1k4@passw0rd";
+	public static final String USERNAME_SALES_SILOAM = "D6200927";
+	public static final String PASSWORD_SALES_SILOAM = "1997-10-23";
 }
